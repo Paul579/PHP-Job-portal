284ee7ea09dda9f8a8945bc56062395fc0df0eb4
Paul579 <pault5504@gmail.com>
2020-04-11 00:09:42 +0530

    First commit

---
+++ joblister/templates/inc/footer.php
@@ -0,0 +1,7 @@
+			<footer class="footer">
+        <p>&copy; INT301 ke017 Inc.</p>
+      </footer>
+
+    </div> 
+		</body>
+</html>
---
+++ joblister/templates/inc/header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>JobListings</title>
+	<link rel = "stylesheet" href="css/journal.css">
+	<link rel = "stylesheet" href="css/styles.css">
+</head>
+<body>
+		<div class="container">
+      <div class="header clearfix">
+        <nav>
+          <ul class="nav nav-pills pull-right">
+            <li role="presentation"><a href="index.php">Home</a></li><hr>
+            <li role="presentation"><a href="create.php">Add post</a></li>
+          </ul>
+        </nav>
+        <h3 class="text-muted">joblistings</h3>
+      </div>
